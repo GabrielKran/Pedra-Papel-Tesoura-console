bacff71bb2e8f72312ac1d526797bde023356f61
GabrielKran <gabrielkran@gmail.com>
2025-03-12 18:59:45 -0300

    Projeto Pedra, Papel, Tesoura terminado (futuras alterações em breve

---
+++ script.js
@@ -0,0 +1,40 @@
+function getComputadorescolha() {
+    return Math.floor(Math.random() * 3) + 1
+
+}
+
+function getHumanEscolha () {
+    let entrada = parseInt(prompt("Digite 1 para Pedra, 2 para Papel e 3 para Tesoura: "))
+    return entrada
+}
+
+let humanScore = 0
+let computadorScore = 0
+
+function playRound() {
+    for (let i = 1; i <= 5; i++) {
+        let hc = getHumanEscolha()
+        let cc = getComputadorescolha()
+
+        if (hc === 1 && cc === 3 || hc === 2 && cc === 1 || hc === 3 && cc === 2) {
+            humanScore ++
+            console.log(`Você ganhou!!\n${humanScore} x ${computadorScore}`)
+            
+        } else if (cc === 1 && hc === 3 || cc === 2 && hc === 1 || cc === 3 && hc === 2) {
+            computadorScore ++
+            console.log(`A máquina ganhou..\n${humanScore} x ${computadorScore}`)
+
+        } else if (hc === 1 && cc === 1 || hc === 2 && cc === 2 || hc === 3 && cc === 3) {
+            console.log(`Empate\n${humanScore} x ${computadorScore}`)
+
+        }
+    }
+
+    if (humanScore > computadorScore) {
+        console.log(`Parabéns, você venceu com ${humanScore} pontos!!`)
+    } else {
+        console.log(`À máquina te venceu com ${computadorScore} pontos..`)
+    }
+}
+
+playRound()
